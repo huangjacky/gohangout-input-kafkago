15e1143482a5582081247772172da3cdaf9f0199
huangjacky <huangjacky@163.com>
2020-12-09 23:13:21 +0800

    1. 增加一个配置SASL来支持SASL

--- gokafka_input.go
+++ gokafka_input.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	js "encoding/json"
+	"net/http"
+	"time"
+
 	"github.com/childe/gohangout/codec"
 	"github.com/golang/glog"
 	kafka_go "github.com/segmentio/kafka-go"
-	"net/http"
-	"time"
+	"github.com/segmentio/kafka-go/sasl"
+	"github.com/segmentio/kafka-go/sasl/plain"
+	"github.com/segmentio/kafka-go/sasl/scram"
 )
 
+// GoKafkaInput 使用的Kafka-go的input插件
 type GoKafkaInput struct {
 	config         map[interface{}]interface{}
 	decorateEvents bool
@@ -19,17 +25,17 @@ type GoKafkaInput struct {
 	readConfig     *kafka_go.ReaderConfig
 }
 
-/**
-增加一个状态获取的接口
+/*
+HTTPKafka 增加一个状态获取的接口
 */
-type HttpKafka struct {
+type HTTPKafka struct {
 	kafka *GoKafkaInput
 }
 
 /**
 返回reader的status接口的数据
 */
-func (h *HttpKafka) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func (h *HTTPKafka) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
 	writer.WriteHeader(http.StatusOK)
 	if h.kafka.reader != nil {
@@ -93,6 +99,62 @@ func (p *GoKafkaInput) getConsumerConfig(config map[interface{}]interface{}) (*k
 	if v, ok := config["RebalanceTimeout"]; ok {
 		c.RebalanceTimeout = time.Duration(v.(int)) * time.Second
 	}
+	var dialer *kafka_go.Dialer
+	if v, ok := config["Timeout"]; ok {
+		i := v.(int)
+		dialer = &kafka_go.Dialer{
+			Timeout:   time.Duration(i) * time.Second,
+			DualStack: true,
+		}
+	} else {
+		dialer = &kafka_go.Dialer{
+			Timeout:   10 * time.Second,
+			DualStack: true,
+		}
+	}
+	if v, ok := config["SASL"]; ok {
+		vh := v.(map[string]interface{})
+		v1, ok1 := vh["Type"]
+		v2, ok2 := vh["Username"]
+		v3, ok3 := vh["Password"]
+		if ok1 && ok2 && ok3 {
+			var (
+				mechanism sasl.Mechanism
+				err       error
+			)
+			switch v1.(string) {
+			case "Plain":
+				mechanism = plain.Mechanism{
+					Username: v2.(string),
+					Password: v3.(string),
+				}
+			case "SCRAM":
+				mechanism, err = scram.Mechanism(
+					scram.SHA512,
+					v2.(string),
+					v3.(string),
+				)
+				if err != nil {
+					glog.Fatal("ERROR FOR SCRAM: ", err)
+				}
+			default:
+				glog.Fatalf("ERROR SASL type: %s", v1.(string))
+			}
+			dialer.SASLMechanism = mechanism
+		} else {
+			glog.Fatal("NO CONFIG FOR SASL")
+		}
+	}
+	//TODO 后面再看是否需要完成了，理论上内网的KAFKA不会开启TLS，毕竟这个耗性能
+	if v, ok := config["TLS"]; ok {
+		vh := v.(map[string]interface{})
+		var tls *tls.Config
+		if _, ok1 := vh["PrivateKey"]; ok1 {
+			glog.Info("TODO")
+		}
+		dialer.TLS = tls
+	}
+	c.Dialer = dialer
 	if err := c.Validate(); err != nil {
 		glog.Fatal("ReadConfig Validate error: ", err)
 		return nil, err
@@ -100,6 +162,9 @@ func (p *GoKafkaInput) getConsumerConfig(config map[interface{}]interface{}) (*k
 	return c, nil
 }
 
+/*
+New 插件模式的初始化
+*/
 func New(config map[interface{}]interface{}) interface{} {
 	p := &GoKafkaInput{
 		messages:       make(chan *kafka_go.Message, 10),
@@ -125,12 +190,12 @@ func New(config map[interface{}]interface{}) interface{} {
 
 	if listen, ok := config["StatsAddr"]; ok {
 		httpAddr := listen.(string)
-		httpKafka := &HttpKafka{
+		HTTPKafka := &HTTPKafka{
 			kafka: p,
 		}
 		go func() {
 			glog.Info("Start Http Server: ", httpAddr)
-			_ = http.ListenAndServe(httpAddr, httpKafka)
+			_ = http.ListenAndServe(httpAddr, HTTPKafka)
 		}()
 
 	}
@@ -147,6 +212,8 @@ func New(config map[interface{}]interface{}) interface{} {
 	}()
 	return p
 }
+
+//ReadOneEvent 单次事件的处理函数
 func (p *GoKafkaInput) ReadOneEvent() map[string]interface{} {
 	message, ok := <-p.messages
 	if ok {
@@ -163,6 +230,8 @@ func (p *GoKafkaInput) ReadOneEvent() map[string]interface{} {
 	}
 	return nil
 }
+
+//Shutdown 关闭需要做的事情
 func (p *GoKafkaInput) Shutdown() {
 	if err := p.reader.Close(); err != nil {
 		glog.Fatal("failed to close reader:", err)
